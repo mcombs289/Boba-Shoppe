d6402eb08bbba7594849037d2d1def2cdc0c368f
Jordy <jordynavalee@gmail.com>
2022-09-22 10:11:36 -0500

    Adds edit/delete into Admin AllProducts view

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchProducts } from "../redux/products";
+import { me } from "../store/auth";
 
 export class AllProducts extends React.Component {
   constructor(props) {
@@ -10,12 +11,36 @@ export class AllProducts extends React.Component {
 
   componentDidMount() {
     this.props.getProducts();
+    this.props.currentUserData();
   }
 
   render() {
     const products = this.props.products || [];
+    const isAdmin = this.props.isAdmin;
 
-    return (
+    return isAdmin ? (
+      <div id="allItems">
+        <h2>Hello</h2>
+        <div>
+          {products.length
+            ? products.map((product) => {
+                return (
+                  <div id="singleItem" key={product.id}>
+                    <div>
+                      <Link to={`/products/${product.id}`}>
+                        <h2>{product.name}</h2>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                      </Link>
+                    </div>
+                    {/* <img style={{ width: "8rem" }} src={product.imageUrl} /> */}
+                  </div>
+                );
+              })
+            : null}
+        </div>
+      </div>
+    ) : (
       <div id="allItems">
         <h2>Hello!</h2>
         <div>
@@ -42,11 +67,15 @@ export class AllProducts extends React.Component {
 const mapState = (state) => {
   return {
     products: state.products,
+    isAdmin: state.auth.adminAccess,
   };
 };
 
 const mapDispatchToProps = (dispatch) => ({
   getProducts: () => dispatch(fetchProducts()),
+  currentUserData() {
+    dispatch(me());
+  },
 });
 
 export default connect(mapState, mapDispatchToProps)(AllProducts);
