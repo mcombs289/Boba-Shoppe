ae4f5b2da18d1308d3cef23dfc621b665e003f9e
Jordy <jordynavalee@gmail.com>
2022-09-24 12:00:31 -0500

    makes StripePayments functional at /checkout

--- client/App.js
+++ client/App.js
@@ -2,11 +2,6 @@ import React from "react";
 
 import Navbar from "./components/Navbar";
 import Routes from "./Routes";
-import { loadStripe } from "@stripe/stripe-js";
-
-const stripePromise = loadStripe(
-  "pk_test_51Ll6xFBPVoNI1kWdYTQYZLfLWHG1A4lwzzWNU6u4CgN0SGXUnzHub3WTeTP9AeUki95aUwS5TuMLxR41aRbty1Xl00SBWkusoc"
-);
 
 const App = () => {
   return (
