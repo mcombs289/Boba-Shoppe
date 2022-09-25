300cc0c67b89e441fa4f9aec213bd0c74cfe96ba
mcombs289 <maddy.combs4@gmail.com>
2022-09-25 00:40:00 -0400

    order summary

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -8,44 +8,56 @@ export class Cart extends React.Component {
   }
   render() {
     let { orders } = this.props || [];
-    let products = orders.products;
+    let products = orders.products || [];
     console.log(products);
     return (
       <div className="cart">
-        {products
-          ? products.map((product) => {
-              return (
-                <div key={product.id}>
-                  <div className="product">
-                    <img src={product.imageUrl} alt="image" />
-                    <div className="productInfo">
-                      <h2> {product.name}</h2>
-                    </div>
-                    <div className="price">
-                      <h3>Item Price: {product.price}</h3>
-                    </div>
-                    <select name="quantity" className="quantSelector">
-                      <option value="1">1</option>
-                      <option value="2">2</option>
-                      <option value="3">3</option>
-                      <option value="4">4</option>
-                      <option value="5">5</option>
-                      <option value="6">6</option>
-                      <option value="7">7</option>
-                      <option value="8">8</option>
-                      <option value="9">9</option>
-                      <option value="10">10</option>
-                    </select>
-                    <div className="price">
-                      <h3>Total Price: {product.price}</h3>
-                    </div>
-                  </div>
+        <h1>Shopping Bag</h1>
+        <hr />
+        <div className="cartInfo">
+          <div className="productsInCart">
+            {products
+              ? products.map((product) => {
+                  return (
+                    <div key={product.id}>
+                      <div className="product">
+                        <img src={product.imageUrl} alt="image" />
+                        <div className="productInfo">
+                          <h2> {product.name}</h2>
+                        </div>
+                        <div className="price">
+                          <h3>Item Price: {product.price}</h3>
+                        </div>
+                        <select name="quantity" className="quantSelector">
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                          <option value="4">4</option>
+                          <option value="5">5</option>
+                          <option value="6">6</option>
+                          <option value="7">7</option>
+                          <option value="8">8</option>
+                          <option value="9">9</option>
+                          <option value="10">10</option>
+                        </select>
+                        <div className="price">
+                          <h3>Total Price: {product.price}</h3>
+                        </div>
+                      </div>
 
-                  <hr />
-                </div>
-              );
-            })
-          : null}
+                      <hr />
+                    </div>
+                  );
+                })
+              : null}
+          </div>
+          <div className="orderSummary">
+            <h2>Order Summmary</h2>
+            <h3>TOTAL</h3>
+            <button className="checkout">Proceed To Checkout</button>
+          </div>
+        </div>
+        <div />
       </div>
     );
   }
